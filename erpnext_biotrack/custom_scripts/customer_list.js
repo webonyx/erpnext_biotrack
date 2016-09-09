2b427b8e500189caf6fc31dd87a0ab998fa783ab
Viet Pham <phpcodervn@gmail.com>
2016-09-09 20:45:15 +0700

    Add customer_group to stat sidebar

--- erpnext_biotrack/custom_scripts/customer_list.js
+++ erpnext_biotrack/custom_scripts/customer_list.js
@@ -1,5 +1,6 @@
 $.extend(frappe.listview_settings['Customer'], {
     onload: function (DocListView) {
+        DocListView.listview.stats.push("customer_group");
         DocListView.page.add_action_item(__("Synchronization"), function () {
             frappe.call({
                 method: "erpnext_biotrack.tasks.client_sync",
